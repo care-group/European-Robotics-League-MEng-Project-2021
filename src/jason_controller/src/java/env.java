03f900667f51430ad2f9a6f97e959b3d45c830cc
Chris Hulme <chris_hulme99@outlook.com>
2021-01-21 09:53:19 +0000

    Added move plan & improved bdi env code structure.

--- src/jason_controller/src/java/env.java
+++ src/jason_controller/src/java/env.java
@@ -1,4 +1,6 @@
-// Environment code for project hello_ros
+// Environment code
+// Start rosbridge to allow Jason to ROS communication:
+// roslaunch rosbridge_server rosbridge_websocket.launch
 
 import jason.asSyntax.*;
 import jason.environment.*;
@@ -14,9 +16,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.util.*;  
 
 
-public class RosEnv extends Environment {
+public class env extends Environment {
 
-    private Logger logger = Logger.getLogger("hello_ros."+RosEnv.class.getName());
+    private Logger logger = Logger.getLogger("jason_controller."+env.class.getName());
     
     RosBridge bridge = new RosBridge();
 
@@ -26,42 +28,45 @@ public class RosEnv extends Environment {
         super.init(args);
 		bridge.connect("ws://localhost:9090", true);
 		logger.info("Environment started, connection with ROS established.");
-		
-		bridge.subscribe(SubscriptionRequestMsg.generate("/ros_to_java")
-				.setType("std_msgs/String")
-				.setThrottleRate(1)
-				.setQueueLength(1),
-			new RosListenDelegate() {
-
-				public void receive(JsonNode data, String stringRep) {
-					MessageUnpacker<PrimitiveMsg<String>> unpacker = new MessageUnpacker<PrimitiveMsg<String>>(PrimitiveMsg.class);
-					PrimitiveMsg<String> msg = unpacker.unpackRosMessage(data);
-					logger.info(msg.data);
-				}
-			}
-	);
     }
 
     @Override
     public boolean executeAction(String agName, Structure action) {
-		if (action.getFunctor().equals("hello_ros")) {
-			hello_ros();
+
+		switch(action.getFunctor()){
+			case "test_ros_communication":
+				test_ros_communication();
+				break;
+			case "at":
+				logger.info("Moving to " + action.getTerm(0));
+				break;
+			default:
+				logger.info("executing: "+action+", but not implemented!");
 		}
-		else {
-			logger.info("executing: "+action+", but not implemented!");
-		}
+		
+		try {
+            Thread.sleep(200);
+        } catch (Exception e) {}
+		
+		updatePercepts();
         informAgsEnvironmentChanged();
         return true; // the action was executed with success
     }
     
-	public void hello_ros() {
-		System.out.println("Publishing..");
+	/** Tests Jason-ROS communication by publishing to the /jason/rotate topic, causing the 
+		robot to briefly spin. */
+	public void test_ros_communication() {
+		logger.info("Testing ROS communication");
 		Publisher pub = new Publisher("/jason/rotate", "std_msgs/Int16", bridge);
 		Map<String,Integer> map=new HashMap<String,Integer>();  
   		map.put("data",5);  
-
 		pub.publish(map);
 	}
+
+    /** creates the agents perception based on the MarsModel */
+	void updatePercepts() {
+        clearPercepts();
+    }
 
     /** Called before the end of MAS execution */
     @Override
