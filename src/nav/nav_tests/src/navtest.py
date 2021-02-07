e50e48162f310651b85fc6735bae764fb3c55bf4
Monteiro <48806912+APMMonteiro@users.noreply.github.com>
2021-02-07 17:21:26 +0000

    added semantic goals, removed utils.py

--- src/nav/nav_tests/src/navtest.py
+++ src/nav/nav_tests/src/navtest.py
@@ -1,32 +1,91 @@
 #! /usr/bin/env python
 
+print("Starting navtest.py")
+
 import rospy
-from utils import *
+import math
+import tf
+from std_msgs.msg import Float64MultiArray
+from geometry_msgs.msg import PoseStamped, Quaternion
 
-rospy.init_node('navigation')
+def quaternion_from_euler(roll, pitch, yaw):
+    '''
+    From HSR's utils.py
+    '''
+    q = tf.transformations.quaternion_from_euler(roll / 180.0 * math.pi,
+                                                 pitch / 180.0 * math.pi,
+                                                 yaw / 180.0 * math.pi, 'rxyz')
+    
+    return Quaternion(q[0], q[1], q[2], q[3])
 
-print("Hi Nav")
 
-semantic_coords = {
-    # coordinates in [x, y, w] 
-    # where w is the direction it is facing in degrees
-    "door" : [-0.416, 0, 0],
-    "drawers" : [0.1, 0, 0],
-    "full desk" : [1.5, 0, 0],
-    "trash" : [2.7, 0, 0],
-    "empty desk" : [0, 1.25, 0],
-    "passage" : [2.65, 2.2, 0],
-    "dude" : [0.4, 2.8, 0],
-    "shelves" : [2.25, 4.4, 0]
-}
+class SimpleMoveBase():
+    ''' 
+    This class is adapted from theconstructsim.com ROS Basics in 5 Days course - Using Python Classes in ROS
+    It implements a pseudo action server to move the HSR to coordinate nav goals 
+    provided through the /azm_nav/coord_goal_listener topic
+    '''
 
-def send_semantic_goal(name, semantics):
-    if name not in semantics:
-        print("That label does not exist in the semantic map")
-        return False
-    t = semantics[name]
-    move_base_goal(t[0], t[1], t[2])
+    def __init__(self, openListenerTopic=True):
+        rospy.loginfo("Initiating basic_coordinate_goal_nav_node")
+        rospy.init_node('basic_semantic_nav_node')
+        self.move_base_simple_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+        self.goal = PoseStamped()
+        self.ctrl_c = False
+        self.rate = rospy.Rate(10) # 10hz
+        rospy.on_shutdown(self.shutdownhook)
+        if openListenerTopic:
+            #start_topic_listener()
+            self.goal_listener = rospy.Subscriber('/azm_nav/coord_goal_listener', Float64MultiArray, self.goal_callback)
+    
+    def publish_once(self):
+        """
+        This is because publishing in topics sometimes fails the first time you publish.
+        In continuous publishing systems, this is no big deal, but in systems that publish only
+        once, it IS very important.
+        """
+        rospy.loginfo("Attempting to publish goal")
+        while not self.ctrl_c:
+            connections = self.move_base_simple_publisher.get_num_connections()
+            if connections > 0:
+                self.move_base_simple_publisher.publish(self.goal)
+                rospy.loginfo("Goal Published")
+                break
+            else:
+                self.rate.sleep()
 
-move_base_goal(2.5, 0.8, 0)
+    def simple_move_base_goal(self, x=0, y=0, theta=0):
+        # Set message variables
+        self.goal.header.frame_id = "map"
+        self.goal.pose.position.x = x
+        self.goal.pose.position.y = y
+        self.goal.pose.orientation = quaternion_from_euler(0, 0, theta)
 
-print("Moved?")
+        # Publish
+        self.publish_once()
+
+    def shutdownhook(self):
+        # works better than the rospy.is_shutdown()
+        self.ctrl_c = True
+
+    def goal_callback(self, msg):
+        ''' Listens for coord goals to move to '''
+        if len(msg.data) != 3:
+            rospy.loginfo("Goal Message received without exactly 3 inputs, ignoring.")
+        else:
+            rospy.loginfo("Goal position received, moving?")
+            self.simple_move_base_goal(msg.data[0], msg.data[1], msg.data[2])
+
+
+def run_move_to_coords(obj, x, y, w):
+    print("sending goal command")
+    obj.simple_move_base_goal(x, y, w)
+
+if __name__ == '__main__':
+    rospy.loginfo("executing navtest.py as main")
+    print("executing navtest.py as main")
+    print("Creating SimpleMoveBase obj")
+    simple_move_obj = SimpleMoveBase()
+    #run_move_to_coords(simple_move_obj, 0, 0, 0)
+    rospy.loginfo("navtest.py is spinning")
+    rospy.spin()
