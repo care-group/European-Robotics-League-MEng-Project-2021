97e328d64dce55d74a8519290198c463cf44be84
Chris Hulme <chris_hulme99@outlook.com>
2021-03-01 10:12:14 +0000

    Face detection now looks for all faces from the camera feed, rather than looking for a specific target.

--- src/cv/simulation_face_detection.py
+++ src/cv/simulation_face_detection.py
@@ -12,6 +12,7 @@ class Face_Detection:
     def __init__(self):
         rospy.init_node('Face_Detection')
         self.bridge = CvBridge()
+        self.results_publisher = rospy.Publisher('/cv/face/personIs', String, queue_size=10)
 
     def subscribe_jason(self):
         jason_subscriber = rospy.Subscriber('/jason/detect_face', String,self.jason_callback)
@@ -24,18 +25,38 @@ class Face_Detection:
     # Takes the current image from the camera feed and searches for the target object, returning coordinates 
     def img_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-        same = self.search_for_face_db(cv_image)
-        if same==True:
-            print("Faces are same")
-        else:
-            print("Different faces")
-
+        # faces = "kimble","postman","deliman","plumber"
+        person = self.get_person_from_face(cv_image)
+        self.publish_results(person)
         self.img_subscriber.unregister()
 
-    def search_for_face_db(self,img):
-        result  = DeepFace.find(img,db_path="images/faces/"+self.target)
-        print(result)
+    def get_person_from_face(self,img):
+        faces = "samuel","bezos","simulated"
+        for face in faces:
+            try:
+                same = self.search_for_face_db(img,"images/faces/"+face)
+                if same==True:
+                    print(face + "'s face detected!")
+                    return face
+                else:
+                    print("Not " + face)
+             # No face detected, return immediately    
+            except ValueError:
+                return None
+
+        return None
+
+    def search_for_face_db(self,img,path):
+        result  = DeepFace.find(img,db_path=path)
         return (result.empty == False)
+
+    def publish_results(self,person):
+
+        if(person!=None):
+            print("publishing that " +person+" is found")
+            self.results_publisher.publish(person)
+        else:
+            self.results_publisher.publish("unknown")
 
 
 face_detection = Face_Detection()
