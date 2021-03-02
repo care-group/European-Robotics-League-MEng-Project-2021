9bdcdaab87cb355e779700c55dcab55d52f74c85
Chris Hulme <chris_hulme99@outlook.com>
2021-03-02 15:19:13 +0000

    Created catkin package for cv, currently implements face detection (py3) and moveHead (py2), although unable to tilt head upwards.

---
+++ src/cv/cv/src/moveHead.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7
+
+import moveit_commander
+import sys
+
+print(sys.executable)
+
+def move_head_tilt(v):
+    head.set_joint_value_target("head_tilt_joint", v)
+    return head.go()
+
+head = moveit_commander.MoveGroupCommander("head")
+move_head_tilt(0)
