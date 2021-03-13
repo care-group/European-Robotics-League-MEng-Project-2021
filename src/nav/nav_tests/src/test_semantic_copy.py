98cb34d631f1b92db62788fbaffc36d14c95e8f2
Monteiro <48806912+APMMonteiro@users.noreply.github.com>
2021-03-13 02:04:10 +0000

    added duplicate check when adding to semantic map

--- src/nav/nav_tests/src/test_semantic_copy.py
+++ src/nav/nav_tests/src/test_semantic_copy.py
@@ -19,6 +19,30 @@ class Test_Map(unittest.TestCase):
         self.assertFalse(self.sem.add_new_to_semantic_map({"name": "", "type":"", "coords": "", "others":{}}))
         # More inputs
         self.assertTrue(self.sem.add_new_to_semantic_map({"name": "", "type":"", "in":"", "coords": [], "others":{}}))
+        # if item already exists
+        self.sem.add_new_to_semantic_map({"name": "test", "type":"", "coords": [1, 2, 3], "others":{}})
+        self.assertTrue(self.sem.add_new_to_semantic_map({"name": "test", "type":"", "coords": [1, 2, 3], "others":{}}))
+        self.assertTrue(self.sem.add_new_to_semantic_map({"name": "test", "type":"", "coords": [2, 2, 3], "others":{}}, 3))
+
+    
+    def test_delete_by_coords(self):
+        # Basic exact
+        self.sem.add_new_to_semantic_map({"name": "", "type":"", "coords": [1, 2, 3], "others":{}})
+        self.assertNotEqual(self.sem.delete_from_semantic_map_by_coords([1, 2, 3]), None)
+        # Tuple test
+        self.sem.add_new_to_semantic_map({"name": "", "type":"", "coords": [1, 2, 3], "others":{}})
+        self.assertNotEqual(self.sem.delete_from_semantic_map_by_coords((1, 2, 3)), None)
+        # Remove something that isn't there
+        self.assertEqual(self.sem.delete_from_semantic_map_by_coords((1000, 1000, 1000)), False)
+        # Not bad inputs
+        self.assertEqual(self.sem.delete_from_semantic_map_by_coords([]), False)
+        self.assertEqual(self.sem.delete_from_semantic_map_by_coords((1000, 1000)), False)
+        # Threshold testing
+        self.sem.add_new_to_semantic_map({"name": "", "type":"", "coords": [1, 2, 3], "others":{}})
+        self.assertNotEqual(self.sem.delete_from_semantic_map_by_coords((1.1, 2, 3)), None)
+        self.sem.add_new_to_semantic_map({"name": "", "type":"", "coords": [2, 2, 3], "others":{}})
+        self.assertNotEqual(self.sem.delete_from_semantic_map_by_coords((1, 2, 3), distance_threshold=3), None)
+
 
 
 if __name__ == '__main__':
