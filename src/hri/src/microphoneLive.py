167e37e8f2360eae065d86fdf0eb160a2a1eff8a
Chris Hulme <chris_hulme99@outlook.com>
2021-04-21 15:33:07 +0000

    Playback

--- src/hri/src/microphoneLive.py
+++ src/hri/src/microphoneLive.py
@@ -4,6 +4,14 @@ from scipy.io.wavfile import write
 fs = 44100  # Sample rate
 seconds = 3  # Duration of recording
 
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+print(sd.query_devices())
+input_device = 2 # CUSTOMISE THIS
+output_device = 1 #CUSTOMISE THIS
+
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1,device=input_device)
 sd.wait()  # Wait until recording is finished
-write('output.wav', fs, myrecording)  # Save as WAV file 
+
+print("starting playback")
+sd.play(myrecording, fs,device=output_device)
+sd.wait()
+print("done")
