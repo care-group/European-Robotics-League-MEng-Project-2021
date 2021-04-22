c57e815bfc7f9843b9b29e357382ee3702054649
Chris Hulme <chris_hulme99@outlook.com>
2021-04-22 11:16:57 +0000

    Created hri package with speech-to-text service and example caller.

---
+++ src/hri/hri/src/speechInput.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sounddevice as sd
+import speech_recognition as sr
+import pyaudio
+
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Empty
+
+from hri.srv import SpeechToText
+
+
+def listenToSpeech(msg):
+    # initialize the recognizer
+    r = sr.Recognizer()
+    #print(sr.Microphone.list_working_microphones())
+    print("Make sure you select the right device (0,1,2...)")
+   
+
+    with sr.Microphone(device_index=2, sample_rate = 48000) as source:
+        # read the audio data from the default microphone
+        print("Listening to audio..")
+        #r.adjust_for_ambient_noise(source)
+        audio = r.listen(source,timeout=2)
+        text =r.recognize_google(audio,language='en-GB')
+        
+        #print(text)
+        output = String()
+        output.data=text
+        return{'rawText':output}
+        
+
+if __name__ == '__main__':
+    rospy.init_node('speechInput')
+    rospy.Service('speechInput',SpeechToText,listenToSpeech)
+    
+    try:
+        rospy.spin()
+    except rospy.ROSException as e:
+        rospy.logerr(e)
